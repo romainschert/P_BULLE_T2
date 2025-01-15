72dbd77fc58f265cea98c9c95542932560e91684
romainschert <150144520+romainschert@users.noreply.github.com>
2025-01-15 16:29:22 +0100

    step5 => factory

---
+++ app-teachers-adonisjs/database/seeders/teacher_seeder.ts
@@ -0,0 +1,33 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Teacher from '#models/teacher'
+export default class extends BaseSeeder {
+  async run() {
+    // Création d'enseignants
+    await Teacher.createMany([
+      {
+        gender: 'M',
+        firstname: 'Grégory',
+        lastname: 'Charmier',
+        nickname: 'GregLeBarbar',
+        origine: "Plateforme de jeux d'échecs",
+        sectionId: 1,
+      },
+      {
+        gender: 'M',
+        firstname: 'Xavier',
+        lastname: 'Carrel',
+        nickname: 'XCL',
+        origine: 'Sigle ETML',
+        sectionId: 1,
+      },
+      {
+        gender: 'W',
+        firstname: 'Aurélie',
+        lastname: 'Curchod',
+        nickname: 'ACD',
+        origine: 'Sigle ETML',
+        sectionId: 1,
+      },
+    ])
+  }
+}
